845ad5ae03880d0629a5b58b3c71573b0d4a5894
Dan <bmdmartinez@gmail.com>
2021-08-01 15:46:08 -0400

    added html reference sheet filled in with sample text

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,13 +1,13 @@
 const Manager = require('../lib/Manager');
 
 test('Creates Manager object', () => {
-    const manager = new Manager('Dan', 7, "bmdmartinez@gmail.com", "001");
+    const manager = new Manager('Dan', 7, "bmdmartinez@gmail.com", 1);
 
-    expect(manager.officeNumber).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 test('Gets updated role', () => {
-    const manager = new Manager('Dan', 7, "bmdmartinez@gmail.com", "001");
+    const manager = new Manager('Dan', 7, "bmdmartinez@gmail.com", 1);
 
     expect(manager.getRole()).toEqual('Manager');
 });
